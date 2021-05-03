43a5c004985721e686658a0da623df379fd40452
willian <willian95prado@gmail.com>
2021-05-03 15:19:58 -0300

    projeto final

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,34 +1,13 @@
 <template>
-    <breeze-authenticated-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </breeze-authenticated-layout>
+  <h1 class="fixed top-45 justfy px-6 py-4 sm:block">Welcome to our page</h1>
 </template>
 
 <script>
-    import BreezeAuthenticatedLayout from '@/Layouts/Authenticated'
+export default {
 
-    export default {
-        components: {
-            BreezeAuthenticatedLayout,
-        },
-
-        props: {
-            auth: Object,
-            errors: Object,
-        },
-    }
+}
 </script>
+
+<style>
+
+</style>
